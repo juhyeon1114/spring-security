82101c53d2becb00fce8531e3d0efb7f1272c274
Kai Kim <juhyeon1114@gmail.com>
2022-12-14 11:11:25 +0900

    security 세팅

---
+++ src/main/java/security/springsecurity/SecurityConfig.java
@@ -0,0 +1,18 @@
+package security.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.formLogin();
+        return http.build();
+    }
+
+}
